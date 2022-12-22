18be5f0973eb7b34b430b36f5965b1f8db9962b3
taserbeat <taserbeat@gmail.com>
2022-12-22 22:32:23 +0900

    feat: 動的コンポーネント

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <script setup lang="ts">
+import { computed, DefineComponent, ref } from "vue";
 import CardList from "./components/CardList.vue";
+import SecondCardList from "./components/SecondCardList.vue";
+
+type TabName = "first" | "second";
+
+const selectedTabMame = ref<TabName>("first");
+
+const updateTab = (tabName: TabName) => {
+  selectedTabMame.value = tabName;
+};
+
+const selectedTab = computed(() => {
+  switch (selectedTabMame.value) {
+    case "first":
+      return CardList;
+
+    case "second":
+      return SecondCardList;
+
+    default:
+      return CardList;
+  }
+});
 </script>
 
 <template>
-  <CardList></CardList>
+  <div class="tab-changer">
+    <button @click="updateTab('first')">Tab 1</button>
+    <button @click="updateTab('second')">Tab 2</button>
+  </div>
+  <div class="tab-contents">
+    <!-- <CardList></CardList> -->
+    <component :is="selectedTab"></component>
+
+    <!-- <CardList v-if="isFirstTab"></CardList>
+    <SecondCardList v-if="!isFirstTab"></SecondCardList> -->
+  </div>
 </template>
 
-<style scoped>
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.tab-contents {
+  margin-top: 20px;
 }
 </style>
